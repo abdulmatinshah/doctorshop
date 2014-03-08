4f3d117bcada2c1891e7f70419d748a790db43b9
abdulmatinshah <abdulmateenshah@gmail.com>
2014-03-08 05:35:36 -0500

    initial commit after activating all default options

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Doctorshop::Application.config.secret_token = '098b85adeddfc77944ca145c8f31768d133fdf36c587058d6d22ee2e554c98f120088c381bbf1833da9cc0e9fe0c3a3f563e6461c235a39db11c5f554190ced8'
